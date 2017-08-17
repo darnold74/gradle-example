bd168240b43ba7fad77163a574fdcb5547f4ce1f
darnold <arnold.dobo@gmail.com>
2017-08-17 14:41:46 +0200

    SecondTest finished

---
+++ src/test/java/SecondTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SecondTest {
+
+    @Test
+    @Disabled
+    void mySecondTest(){
+        assertEquals(2, 1, "2 is not equal to 1");
+    }
+
+    @Test
+    @Tag("slow")
+    void aSlowTest() throws InterruptedException {
+        Thread.sleep(1000);
+    }
+}
