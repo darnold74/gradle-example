0e2562b9bdadfdc8fa4558e74bafa4d9c918812a
darnold <arnold.dobo@gmail.com>
2017-08-17 14:35:58 +0200

    FirstTest finished

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Tag("fast")
 public class FirstTest {
+
+    @Test
+    @DisplayName("My 1st JUnit 5 test!")
+    void myFirstTest(TestInfo testInfo){
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.add(1,1), "1 + 1 shoud equal 2");
+        assertEquals("My 1st JUnit 5 test!", testInfo.getDisplayName(), () -> "TestInfo is injected correctly");
+    }
 }
